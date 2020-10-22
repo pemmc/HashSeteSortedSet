99d87ec78295f97e5f6bd0a5232d399a64fec985
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-22 17:21:11 -0300

    Ele testa o gethashcode e depois se existe ele usa o equals para confirmar!

--- HashSeteSortedSet/Program.cs
+++ HashSeteSortedSet/Program.cs
@@ -8,46 +8,15 @@ namespace HashSeteSortedSet
     {
         static void Main(string[] args)
         {
-            //OPERAÇÕES ALGÉBRICAS
-            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
-            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+            HashSet<string> set = new HashSet<string>();
 
-            PrintCollection(a); //Resultado: 0 2 4 5 6 8 10
+            set.Add("Maria");
+            set.Add("Alex");
 
-            //União
-
-            SortedSet<int> c = new SortedSet<int>(a);
-
-            c.UnionWith(b);
-
-            PrintCollection(c); //Resultado: 0 2 4 5 6 7 8 9 10 
-
-            //Interseção
-
-            SortedSet<int> d = new SortedSet<int>(a);
-            d.IntersectWith(b);
-
-            PrintCollection(d); //Resultado: 5 6 8 10
-
-            //Diferenca
-            SortedSet<int> e = new SortedSet<int>(a);
-            e.ExceptWith(b);
-
-
-            PrintCollection(e); //Resultado: 0 2 4
-        }
-
-        static void PrintCollection<T>(IEnumerable<T> collection){
-            foreach(T obj in collection)
-            {
-                Console.Write(obj + " ");
-
-            }
-
-            Console.WriteLine();
+            //Ele testa o gethashcode e depois se existe ele usa o equals para confirmar!
+            Console.WriteLine(set.Contains("Maria"));
 
         }
-
 
     }
 }
