229335e08e981d044b626cd173ea1377eecc843b
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-22 16:56:23 -0300

    Comecando com o conjunto  HashSet<T>

---
+++ HashSeteSortedSet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace HashSeteSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Instanciamos nosso conjuunto na memória
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook")); //True
+            Console.WriteLine(set.Contains("SmartPhone")); //False
+
+            foreach(string p in set)
+            {
+                Console.WriteLine(p);
+
+            }
+        }
+    }
+}
